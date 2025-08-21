d14d81512146dce57dc0d10e65209a8ca3c779a7
Raja Krishnaswamy <rjthescholar@gmail.com>
2025-08-21 03:02:22 -0400

    more

---
+++ dataset_doccano/fmt_files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -evx
+cd data/dataset
+for dir in */; do
+    if [ -d "$dir" ]; then
+        mkdir ../dataset_conll/$dir -p
+		mkdir ../dataset_bio/$dir -p
+        mkdir ../concepts_bio/$dir -p
+		cd "$dir"
+		for course in */; do
+            if [ -d "$course" ]; then
+                mkdir ../../dataset_conll/$dir/$course -p
+                mkdir ../../dataset_bio/$dir/$course -p
+                mkdir ../../concepts_bio/$dir/$course -p
+                cd "$course"
+                for file in *.jsonl; do
+                    #../../../../to_conll.py -f $file -o ../../../dataset_conll/$dir$course"${file%.*}".conll
+                    #../../../../to_bio_json.py -f "../../../dataset_conll/$dir$course${file%.*}".conll -o ../../../dataset_bio/$dir$course"${file%.*}".json
+                    ../../../../concept_scripts/extract_concepts.py -f "../../../dataset_bio/$dir$course${file%.*}".json -o ../../../concepts_bio/$dir$course"${file%.*}".txt
+                done
+            fi
+            cd ..
+        done
+        cd ..
+    fi
+done
