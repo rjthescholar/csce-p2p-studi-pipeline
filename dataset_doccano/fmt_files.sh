d8714605a7a008cb7bd3079151ab0c4d31c64535
Raja Krishnaswamy <rjthescholar@gmail.com>
2025-09-16 11:12:28 -0400

    removed original slides

--- dataset_doccano/fmt_files.sh
+++ dataset_doccano/fmt_files.sh
@@ -13,12 +13,13 @@ for dir in */; do
                 mkdir ../../dataset_bio/$dir/$course -p
                 mkdir ../../dataset_bio_uniform/$dir/$course -p
                 mkdir ../../concepts_bio/$dir/$course -p
+                # mkdir ../../dataset_bio/distant/$course -p
                 cd "$course"
                 for file in *.jsonl; do
-                    #../../../../to_conll.py -f $file -o ../../../dataset_conll/$dir$course"${file%.*}".conll
-                    #../../../../to_bio_json.py -f "../../../dataset_conll/$dir$course${file%.*}".conll -o ../../../dataset_bio/$dir$course"${file%.*}".json
-                    #../../../../concept_scripts/extract_concepts.py -f "../../../dataset_bio/$dir$course${file%.*}".json -o ../../../concepts_bio/$dir$course"${file%.*}".txt
-                    ../../../../concept_scripts/distant_label.py -f "../../../dataset_bio/$dir$course${file%.*}".json -o "../../../dataset_bio_uniform/$dir$course${file%.*}".json -d ../../../concepts_bio/$dir$course"${file%.*}".txt
+                    ../../../../to_conll.py -f $file -o ../../../dataset_conll/$dir$course"${file%.*}".conll
+                    ../../../../to_bio_json.py -f "../../../dataset_conll/$dir$course${file%.*}".conll -o ../../../dataset_bio/$dir$course"${file%.*}".json
+                    ../../../../concept_scripts/extract_concepts.py -f "../../../dataset_bio/$dir$course${file%.*}".json -o ../../../concepts_bio/$dir$course"${file%.*}".txt
+                    #../../../../concept_scripts/distant_label.py -f "../../../dataset_bio/unlabeled/$course${file%.*}".json -o "../../../dataset_bio/distant/$dir$course${file%.*}".json -d ../../../concepts/distant/$course"${file%.*}".json
                 done
             fi
             cd ..
